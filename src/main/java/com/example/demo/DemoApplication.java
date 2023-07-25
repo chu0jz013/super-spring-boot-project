8f32c20776069c0cd85833fca9e8274e5bce83a6
quachuoizbietcode <84366779+chu0jz013@users.noreply.github.com>
2023-07-25 16:59:34 +0700

    Update DemoApplication.java 6

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World test cicd pipeline5");
+		System.out.println("Hello World test cicd pipeline666");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
